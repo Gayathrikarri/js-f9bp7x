51ba11fa037c7dc8a4a17a38d372fe05c0041590
Gayathrikarri <107988585+Gayathrikarri@users.noreply.github.com>
2023-02-21 13:19:17 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-ReactDOM.render(<h1>HELLO!</h1>,document.getElementById('hi'));
+ReactDOM.render(<h1>HELLO!</h1>, document.getElementById('app'));
